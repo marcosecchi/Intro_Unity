c01a531ae295e228367384760954ef07e68e031f
marcosecchi <loziosecchi@gmail.com>
2026-02-06 11:12:26 +0100

    Add long press jump.

--- Assets/Scripts/MarioController.cs
+++ Assets/Scripts/MarioController.cs
@@ -6,11 +6,14 @@ public class MarioController : MonoBehaviour
 
     public float speed = 1.0f;
     public float jumpForce = 1.0f;
+    public float longPressLimit = 1.0f;
     public KeyCode jumpKey = KeyCode.Space;
 
     public ForceMode forceMode;
 
     bool _isJumping;
+
+    float _duration = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,11 +30,25 @@ public class MarioController : MonoBehaviour
 
         if(Input.GetKeyDown(jumpKey) && !_isJumping)
         {
-            _isJumping = true;
-            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            _duration = 0;
         }
+        else if (Input.GetKeyUp(jumpKey) && !_isJumping)
+        {
+            _isJumping = true;
 
-        Debug.Log(rb.linearVelocity.y);
+            _duration = Mathf.Clamp(_duration, 0, longPressLimit);
+            // E' uguale a
+            //if (_duration > longPressLimit)
+            //{
+              //  _duration = longPressLimit;
+            //}
+            
+            float jumpIntensity = _duration * jumpForce;
+            rb.AddForce(0, jumpIntensity, 0, ForceMode.Impulse);
+        }
+        _duration += Time.deltaTime;
+
+           Debug.Log(_duration);
     }
 
     void OnCollisionEnter(Collision collision)
